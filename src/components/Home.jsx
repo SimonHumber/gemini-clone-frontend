edc377e8464871cf83a4865a2935419a56c071ce
simon <simontran18@gmail.com>
2024-08-27 00:33:46 -0400

    fix error handling, give submit button different shades depending on prompt/hover

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ import "./styles.css";
 const Home = () => {
   const [prompt, setPrompt] = useState("");
   const [messageHistory, setMessageHistory] = useState([]);
-  const messagesEndRef = useRef(null); // Create a ref for scrolling
+  const messagesEndRef = useRef(null); // Create a ref for auto scrolling
 
   const handleSubmit = () => {
     console.log(prompt);
@@ -26,6 +26,7 @@ const Home = () => {
         socket.send(prompt); // Sending a prompt
       });
 
+      //custom event, backend emits response
       socket.on("response", (data) => {
         console.log("Received:", data);
         setMessageHistory((prevHistory) => [...prevHistory, data]); //append response to message history
@@ -37,17 +38,16 @@ const Home = () => {
         console.log("Connection closed");
       });
 
-      socket.on("error", (error) => {
-        console.error("Socket.IO Error:", error);
+      //if backend is off, this will run
+      socket.on("connect_error", (error) => {
+        console.error(error);
       });
-
-      // Await the response from the promise
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
-  const handleKeyDown = (event) => {
+  const handleEnterKey = (event) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault(); // Prevent new line
       handleSubmit(); // Call submit function
@@ -61,8 +61,7 @@ const Home = () => {
 
   return (
     <div className="font-sans bg-[#212121] flex flex-col items-center h-screen m-0">
-      {/* sidebar here */}
-      <div className="p-5 w-full md:w-8/12 lg:w-7/12 h-11/12 flex flex-col ">
+      <div className="p-5gw-full md:w-8/12 lg:w-7/12 h-11/12 flex flex-col ">
         <header className="w-full text-3xl text-[#dadada] mb-5 text-center pb-10 border-b border-gray-600 m-auto">
           CPAN226CHAT
         </header>
@@ -96,21 +95,27 @@ const Home = () => {
         </div>
         <div className="w-full m-auto flex items-center pt-2 border-t border-[#a5a5a5]">
           <textarea
-            className="w-full p-2 pl-5 mr-2 bg-[#121212] text-[#fbfbfb] text-xl rounded-full"
+            className="w-full p-2 pl-5 mr-1 bg-[#121212] text-[#fbfbfb] text-xl rounded-full"
             placeholder="Ask ChatGPT..."
             value={prompt}
             onChange={(event) => setPrompt(event.target.value)}
-            onKeyDown={handleKeyDown} // Handle key events
-            rows="2" // Adjust rows as needed
-            style={{ resize: "none" }} //remove resizing button on bottom right corner
+            onKeyDown={handleEnterKey}
+            rows="2"
+            style={{ resize: "none" }}
+            aria-label="Enter prompt here"
           />
           <button
             id="send"
-            className="cursor-pointer rounded bg-none border-none flex justify-center items-center p-1"
+            className={`p-1 ${
+              prompt === ""
+                ? "opacity-20 cursor-default"
+                : "hover:brightness-50"
+            }`}
             onClick={handleSubmit}
-            disabled={prompt === ""} //if textarea empty, cannot submit
+            disabled={prompt === ""}
+            aria-label="Send message"
           >
-            <img src={sendArrow} alt="Send" className="w-5 h-5" />
+            <img src={sendArrow} alt="Send" className="w-7 h-7" />
           </button>
         </div>
       </div>
